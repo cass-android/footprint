0738aa79c564f20f061a5e7d3a2217328f77a363
Cassandra Heide <cassandraheide@gmail.com>
2019-08-04 11:43:56 -0400

    replace y axis with secondary axis

--- views.py
+++ views.py
@@ -11,7 +11,18 @@ import json
 
 @app.route('/', methods=['GET'])
 def index():
+
+    months = ['Jan','Feb','Mar','Apr','May','Jun',
+     'Jul','Aug','Sep','Oct','Nov','Dec']
+
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
     df = pd.read_csv("static/footprintnetwork.csv")
+    df2 = pd.DataFrame({'month':months, 'value':values})
+
     chart_data = json.dumps(df.to_dict(orient='records'), indent=2)
-    data = {'chart_data':chart_data }
+    ylabel_data = json.dumps(df2.to_dict(orient='records'), indent=2)
+
+    data = {'chart_data':chart_data, 'ylabel_data':ylabel_data }
+
     return render_template('index.html', data=data)
