082e7fabbb4a7152e95d46072971744331323287
Cassandra Heide <cassandraheide@gmail.com>
2019-08-04 00:11:12 -0400

    it's ugly but it works

--- app.py
+++ app.py
@@ -10,6 +10,3 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ use this to run the app
 """
 from app import app
 from views import *
-from models import *
 import os
 
 if __name__ == '__main__':
