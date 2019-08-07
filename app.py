aad2aed001545dbf6eaf079915ad918366e4778b
Cassandra Heide <cassandraheide@gmail.com>
2019-08-07 00:14:07 -0400

    requirements, config,remove sqlalchemy

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ avoid circular imports
 """
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
